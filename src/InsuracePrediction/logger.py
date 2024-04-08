510db3bad5799dc6238ceabbfaf31920d1705fa4
vinayak1514 <vinayakjangir1514@gmail.com>
2024-04-08 13:34:33 +0530

    add logger file

---
+++ src/InsuracePrediction/logger.py
@@ -0,0 +1,23 @@
+import os 
+import logging
+from datetime import datetime
+
+# Creating log file with timestamp
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Creating directory for logs if it doesn't exist
+log_path = os.path.join(os.getcwd(),"logs")
+os.makedirs(log_path,exist_ok=True)
+
+# Setting log file path
+
+LOG_FILEPATH = os.path.join(log_path,LOG_FILE)
+
+
+# Configuring basic logging settings
+logging.basicConfig(level=logging.INFO, 
+                    filename=LOG_FILEPATH,
+                    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+)
+if __name__ == '__main__':
+    logging.info("Here again, I am testing")
